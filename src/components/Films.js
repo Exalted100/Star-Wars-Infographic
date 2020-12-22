7af1cba92bac08f9fb686fa1e25b8d670f3dd145
Fawaz Haroun <harounayomikun@gmail.com>
2020-12-22 21:58:44 +0100

    new styles

--- src/components/Films.js
+++ src/components/Films.js
@@ -1,32 +1,34 @@
-import React from "react"
+import React, { Component } from "react"
 import "./Stylesheets/Films.css"
 import StarwarsContext from "../Contexts/StarwarsContext"
 
-const Films = () => {
-    return (
-        <div>
-            <h1>STAR WARS MOVIES</h1>
-            <div className="films-container" >
-                <StarwarsContext.Consumer>
-                    {(value) => {
-                        if (!value.films) {
-                            return "No films are available at this time"
-                        }  else {
-                            return value.films.data.results.map(x => {
-                                return <div className="films" key={x.title}>
-                                    <div className="films-inner">
-                                        <p className="title">{x.title}</p>
-                                        <p className="director">{x.director}</p>
-                                        <p>{x.producer}</p>
-                                        <p>{x.release_date}</p>
+class Films extends Component{
+    render() {
+        return (
+            <div>
+                <h1>STAR WARS MOVIES</h1>
+                <div className="films-container" >
+                    <StarwarsContext.Consumer>
+                        {(value) => {
+                            if (!value.films) {
+                                return "No films are available at this time"
+                            }  else {
+                                return value.films.data.results.map(x => {
+                                    return <div className="films" key={x.title}>
+                                        <div className="films-inner">
+                                            <p className="title">{x.title}</p>
+                                            <p className="director">{x.director}</p>
+                                            <p>{x.producer}</p>
+                                            <p>{x.release_date}</p>
+                                        </div>
                                     </div>
-                                </div>
-                        }
-                            )}}}
-                </StarwarsContext.Consumer>
+                            }
+                                )}}}
+                    </StarwarsContext.Consumer>
+                </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
+} 
 
 export default Films
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Footer extends Component {
     render() {
         return (
-            <div style={{textAlign: "center", borderTop: "black 2px solid", marginTop: "5em"}}>
+            <div style={{textAlign: "center", marginTop: "5em"}}>
                 <p>Copyright Fawaz Haroun 2020</p>
             </div>
         )
